6d45756bfe0cd7b51f40335b9fcc55bfe49347af
Sinan Caliskan <42204545+codersinan@users.noreply.github.com>
2020-02-18 16:34:22 +0300

    fix sonarqube codesmell and remove current path parameter on sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 token="$(cat sonar-token.txt)"
 dir="$(pwd)"
-echo ${dir}
 dotnet sonarscanner begin /k:"IdentityServer" /d:sonar.host.url="http://localhost:9000" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.coverage.exclusions="*.Tests/**,**/*Tests.cs" /d:sonar.cs.opencover.reportsPaths="${dir}/lcov.opencover.xml"
 dotnet restore
 dotnet build
