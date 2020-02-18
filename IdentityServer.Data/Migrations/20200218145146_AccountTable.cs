18a554208c75414fe08441337be146a107eb0e73
Sinan Caliskan <42204545+codersinan@users.noreply.github.com>
2020-02-18 18:23:10 +0300

    add AccountTable migration

---
+++ IdentityServer.Data/Migrations/20200218145146_AccountTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.Data.Migrations
+{
+    public partial class AccountTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    LastModifiedAt = table.Column<DateTime>(nullable: true),
+                    Username = table.Column<string>(nullable: false),
+                    UserMail = table.Column<string>(nullable: false),
+                    PasswordSalt = table.Column<string>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.UniqueConstraint("AK_Accounts_Username_UserMail", x => new { x.Username, x.UserMail });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
